30ef8e3ec468170cbcfd4adf16a74395d1270361
AleksandrMK <jasonc@mail.ru>
2023-03-09 19:21:42 +0400

    add path accounts

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
    path('admin/', admin.site.urls),
    path('pages/', include('django.contrib.flatpages.urls')),
    path('', include('news.urls')),
-   path('accounts/', include('django.contrib.auth.urls'))
+   path('accounts/', include('django.contrib.auth.urls')),
+   path('accounts/', include("accounts.urls")),
 ]
