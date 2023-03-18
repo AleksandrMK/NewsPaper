c98eddd5113e9c50575b89b1dcddf25b97e06cdd
AleksandrMK <jasonc@mail.ru>
2023-03-18 14:29:16 +0400

    add subscribers form

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -24,6 +24,7 @@ class Author(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
+    subscribers = models.ManyToManyField(User, related_name='categories') # add form subscribers
 
     def __str__(self):
         return f'id={self.pk} name={self.name}'
