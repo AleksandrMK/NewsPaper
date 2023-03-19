f9bde7ac067145cb6a71a7b3944034d60ea109f5
AleksandrMK <jasonc@mail.ru>
2023-03-19 17:03:12 +0400

    edit code to send a mail

--- NewsPaper/accounts/forms.py
+++ NewsPaper/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User, Group
 from allauth.account.forms import SignupForm
+from django.core.mail import send_mail
 
 
 class SignUpForm(UserCreationForm):
@@ -24,6 +25,12 @@ class SignUpForm(UserCreationForm):
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
-        common_users = Group.objects.get(name="common users")
-        user.groups.add(common_users)
+
+        send_mail(
+            subject='Добро пожаловать на наш новостной портал!',
+            message=f'{user.username}, вы успешно зарегистрировались',
+            from_email=None,  # будет спользовано значение DEFAULT_FROM_EMAIL
+            recipient_list=[user.email],
+        )
         return user
+
